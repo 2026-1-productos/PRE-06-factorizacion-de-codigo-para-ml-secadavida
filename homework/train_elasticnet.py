c240921db86a1a21c885c6db102181c6fae3b064
secadavida <142696326+secadavida@users.noreply.github.com>
2026-05-23 12:13:03 -0500

    feat: homework

--- homework/train_elasticnet.py
+++ homework/train_elasticnet.py
@@ -9,55 +9,71 @@
 #
 
 # importacion de librerias
+import os
+
+import joblib
 import pandas as pd
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
+
+def print_metrics(mse, mae, r2, title):
+    print()
+    print(title)
+    print(f"  MSE: {mse}")
+    print(f"  MAE: {mae}")
+    print(f"  R2: {r2}")
+
+
+def calculate_metrics(x_train, y_train, estimator):
+    y_pred = estimator.predict(x_train)
+    mse = mean_squared_error(y_train, y_pred)
+    mae = mean_absolute_error(y_train, y_pred)
+    r2 = r2_score(y_train, y_pred)
+    return mse, mae, r2
+
+
 # descarga de datos
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-df = pd.read_csv(url, sep=";")
+def prepare_data():
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    df = pd.read_csv(url, sep=";")
 
-# preparacion de datos
-y = df["quality"]
-x = df.copy()
-x.pop("quality")
+    # preparacion de datos
+    y = df["quality"]
+    x = df.copy()
+    x.pop("quality")
 
-# dividir los datos en entrenamiento y testing
-(x_train, x_test, y_train, y_test) = train_test_split(
-    x,
-    y,
-    test_size=0.25,
-    random_state=123456,
-)
+    # dividir los datos en entrenamiento y testing
+    (x_train, x_test, y_train, y_test) = train_test_split(
+        x,
+        y,
+        test_size=0.25,
+        random_state=123456,
+    )
+
+    return x_train, x_test, y_train, y_test
+
+
+x_train, x_test, y_train, y_test = prepare_data()
 
 # entrenar el modelo
 estimator = ElasticNet(alpha=0.5, l1_ratio=0.5, random_state=12345)
 estimator.fit(x_train, y_train)
 
+os.makedirs("models", exist_ok=True)
+joblib.dump(estimator, "models/estimator.pkl")
+
 print()
 print(estimator, ":", sep="")
 
 # Metricas de error durante entrenamiento
-y_pred = estimator.predict(x_train)
-mse = mean_squared_error(y_train, y_pred)
-mae = mean_absolute_error(y_train, y_pred)
-r2 = r2_score(y_train, y_pred)
 
-print()
-print("Metricas de entrenamiento:")
-print(f"  MSE: {mse}")
-print(f"  MAE: {mae}")
-print(f"  R2: {r2}")
+mse, mae, r2 = calculate_metrics(x_train, y_train, estimator)
+print_metrics(mse, mae, r2, title="Metricas de entrenamiento:")
+
 
 # Metricas de error durante testing
-print()
-print("Metricas de testing:")
-y_pred = estimator.predict(x_test)
-mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
 
-print(f"  MSE: {mse}")
-print(f"  MAE: {mae}")
-print(f"  R2: {r2}")
+mse, mae, r2 = calculate_metrics(x_test, y_test, estimator)
+print_metrics(mse, mae, r2, title="Metricas de testing:")
--- homework/train_knn.py
+++ homework/train_knn.py
@@ -6,27 +6,11 @@
 #
 
 # importacion de librerias
-import pandas as pd
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 
-# descarga de datos
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-df = pd.read_csv(url, sep=";")
+from train_elasticnet import calculate_metrics, prepare_data, print_metrics
 
-# preparacion de datos
-y = df["quality"]
-x = df.copy()
-x.pop("quality")
-
-# dividir los datos en entrenamiento y testing
-(x_train, x_test, y_train, y_test) = train_test_split(
-    x,
-    y,
-    test_size=0.25,
-    random_state=123456,
-)
+x_train, x_test, y_train, y_test = prepare_data()
 
 # entrenar el modelo
 estimator = KNeighborsRegressor(n_neighbors=5)
@@ -36,25 +20,9 @@ print()
 print(estimator, ":", sep="")
 
 # Metricas de error durante entrenamiento
-y_pred = estimator.predict(x_train)
-mse = mean_squared_error(y_train, y_pred)
-mae = mean_absolute_error(y_train, y_pred)
-r2 = r2_score(y_train, y_pred)
-
-print()
-print("Metricas de entrenamiento:")
-print(f"  MSE: {mse}")
-print(f"  MAE: {mae}")
-print(f"  R2: {r2}")
+mse, mae, r2 = calculate_metrics(x_train, y_train, estimator)
+print_metrics(mse, mae, r2, title="Metricas de entrenamiento:")
 
 # Metricas de error durante testing
-print()
-print("Metricas de testing:")
-y_pred = estimator.predict(x_test)
-mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-print(f"  MSE: {mse}")
-print(f"  MAE: {mae}")
-print(f"  R2: {r2}")
+mse, mae, r2 = calculate_metrics(x_test, y_test, estimator)
+print_metrics(mse, mae, r2, title="Metricas de testing:")
